d50ffa70b77db86be41ba7eb8a41de4364e89bd8
Cristian Lara <cristian@angazadesign.com>
2026-02-07 15:39:13 -0500

    lint

--- js/pokemon.js
+++ js/pokemon.js
@@ -9,7 +9,7 @@ var createdPokemon = [];
  * rand()
  * returns a random integer between min (inclusive) and max (inclusive)
  */
-var rand = function(min, max) {
+var rand = function (min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
@@ -19,11 +19,11 @@ var rand = function(min, max) {
  * pokemon names. Remove the name from the list to not
  * have duplicates.
  */
-var createAllPokemon = function() {
+var createAllPokemon = function () {
 	while (pokemonNames.length > 0) {
 		createdPokemon.push(
 			new Pokemon(
-				pokemonNames.splice(rand(0, pokemonNames.length-1), 1)[0]
+				pokemonNames.splice(rand(0, pokemonNames.length - 1), 1)[0]
 			)
 		);
 	}
@@ -35,11 +35,11 @@ var createAllPokemon = function() {
  * pokemon names. Remove the name from the list to not
  * have duplicates.
  */
-var createSinglePokemon = function() {
+var createSinglePokemon = function () {
 	if (pokemonNames.length) {
 		createdPokemon.push(
 			new Pokemon(
-				pokemonNames.splice(rand(0, pokemonNames.length-1), 1)[0]
+				pokemonNames.splice(rand(0, pokemonNames.length - 1), 1)[0]
 			)
 		);
 	}
@@ -65,17 +65,17 @@ function Pokemon(name) {
 
 	// keycode constants
 	this.directionText = Object.freeze({
-		37:"left", 38:"up", 39:"right", 40:"down"
+		37: "left", 38: "up", 39: "right", 40: "down"
 	});
 	this.keycode = Object.freeze({
-		LEFT: 37, UP: 38, RIGHT: 39, DOWN:40
+		LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40
 	});
 
 	// add pokemon to DOM
 	$("#title").before(
 		"<img id=\"" + name + "\" "
-		+    "class=\"" + name + "\" "
-		+    "src=\"img/pokemon/" + name + "/left0.png\">"
+		+ "class=\"" + name + "\" "
+		+ "src=\"img/pokemon/" + name + "/left0.png\">"
 	);
 
 	this.sprite = $("#" + name)[0];
@@ -87,8 +87,8 @@ function Pokemon(name) {
  * animateIn()
  * animate the pokemon into view from a random location
  */
-Pokemon.prototype.calculatePositions = function() {
-	var top  = window.pageYOffset || document.documentElement.scrollTop;
+Pokemon.prototype.calculatePositions = function () {
+	var top = window.pageYOffset || document.documentElement.scrollTop;
 	var left = window.pageXOffset || document.documentElement.scrollLeft;
 	var height = $(window).height();
 	var width = window.innerWidth;
@@ -96,7 +96,7 @@ Pokemon.prototype.calculatePositions = function() {
 	this.posY = rand(top + this.margin, top + height - this.sprite.getBoundingClientRect().height - this.margin);
 	var targetX;
 
-	if (rand(0,1)) {
+	if (rand(0, 1)) {
 		// animate from right
 		this.posX = width;
 		this.direction = this.keycode.LEFT;
@@ -108,25 +108,25 @@ Pokemon.prototype.calculatePositions = function() {
 		targetX = rand(this.margin, (width / 2) - this.sprite.style.width - this.margin);
 	}
 
-	this.sprite.style.left =this.posX +'px';
-	this.sprite.style.top = this.posY +'px';
+	this.sprite.style.left = this.posX + 'px';
+	this.sprite.style.top = this.posY + 'px';
 
 	// var targetX = rand(this.margin, width - this.sprite.style.width - this.margin);
 	this.walkIn(targetX);
 };
 
-Pokemon.prototype.walkIn = function(target) {
-	if(this.direction == this.keycode.LEFT) {
-		if(this.posX > target) {
+Pokemon.prototype.walkIn = function (target) {
+	if (this.direction == this.keycode.LEFT) {
+		if (this.posX > target) {
 			// haven't reached the target yet
 			this.animStep = (this.animStep + 1) % 2;
 			this.sprite.src = "img/pokemon/" + this.name + "/"
-			+  this.directionText[this.direction]
-			+  this.animStep % 2 + ".png";
+				+ this.directionText[this.direction]
+				+ this.animStep % 2 + ".png";
 			this.posX -= this.stepSize;
 			this.sprite.style.left = this.posX + 'px';
 
-			setTimeout(function(target, ref) {
+			setTimeout(function (target, ref) {
 				ref.walkIn(target);
 			}, 100, target, this);
 		} else {
@@ -134,16 +134,16 @@ Pokemon.prototype.walkIn = function(target) {
 			this.startAnimating();
 		}
 	} else {
-		if(this.posX < target) {
+		if (this.posX < target) {
 			// haven't reached the target yet
 			this.animStep = (this.animStep + 1) % 2;
 			this.sprite.src = "img/pokemon/" + this.name + "/"
-			+  this.directionText[this.direction]
-			+  this.animStep % 2 + ".png";
+				+ this.directionText[this.direction]
+				+ this.animStep % 2 + ".png";
 			this.posX += this.stepSize;
 			this.sprite.style.left = this.posX + 'px';
 
-			setTimeout(function(target, ref) {
+			setTimeout(function (target, ref) {
 				ref.walkIn(target);
 			}, 100, target, this);
 		} else {
@@ -157,8 +157,8 @@ Pokemon.prototype.walkIn = function(target) {
  * takeStep()
  * change direction and location of sprite
  */
-Pokemon.prototype.takeStep = function() {
-	this.direction = rand(37,40);
+Pokemon.prototype.takeStep = function () {
+	this.direction = rand(37, 40);
 	this.walking = true;
 };
 
@@ -167,53 +167,53 @@ Pokemon.prototype.takeStep = function() {
  * auto walking animation cycling through the sprite in the
  * current direction
  */
-Pokemon.prototype.animate = function() {
+Pokemon.prototype.animate = function () {
 	this.animStep = (this.animStep + 1) % 2;
 	this.sprite.src = "img/pokemon/" + this.name + "/"
-	+  this.directionText[this.direction]
-	+  this.animStep % 2 + ".png";
+		+ this.directionText[this.direction]
+		+ this.animStep % 2 + ".png";
 
-	if(this.walking) {
+	if (this.walking) {
 		this.step = (this.step + 1) % 2;
-		switch(this.direction) {
+		switch (this.direction) {
 			case this.keycode.RIGHT:
-			this.posX += this.stepSize;
-			break;
+				this.posX += this.stepSize;
+				break;
 
 			case this.keycode.LEFT:
-			this.posX -= this.stepSize;
-			break;
+				this.posX -= this.stepSize;
+				break;
 
 			case this.keycode.DOWN:
-			this.posY += this.stepSize;
-			break;
+				this.posY += this.stepSize;
+				break;
 
 			case this.keycode.UP:
-			this.posY -= this.stepSize;
-			break;
+				this.posY -= this.stepSize;
+				break;
 
 			default: break;
 		}
 
-		if(document.getElementById(this.name).getBoundingClientRect().top > $(window).height()) {
+		if (document.getElementById(this.name).getBoundingClientRect().top > $(window).height()) {
 			this.posY -= $(window).height();
 		}
-		if(document.getElementById(this.name).getBoundingClientRect().left > $(window).width()) {
+		if (document.getElementById(this.name).getBoundingClientRect().left > $(window).width()) {
 			this.posX -= $(window).width();
 		}
-		if(document.getElementById(this.name).getBoundingClientRect().top < 0) {
+		if (document.getElementById(this.name).getBoundingClientRect().top < 0) {
 			this.posY += $(window).height();
 		}
-		if(document.getElementById(this.name).getBoundingClientRect().left < 0) {
+		if (document.getElementById(this.name).getBoundingClientRect().left < 0) {
 			this.posX += $(window).width();
 		}
 
-		document.getElementById(this.name).style.left =this.posX + 'px';
+		document.getElementById(this.name).style.left = this.posX + 'px';
 		document.getElementById(this.name).style.top = this.posY + 'px';
 
-		if(this.step == 0) {
+		if (this.step == 0) {
 			this.walking = false;
-			setTimeout(function(ref) { ref.takeStep(); }, rand(1000,3000), this);
+			setTimeout(function (ref) { ref.takeStep(); }, rand(1000, 3000), this);
 		}
 	}
 };
@@ -222,7 +222,7 @@ Pokemon.prototype.animate = function() {
  * stopWalking()
  * pause the walking animation timer
  */
-Pokemon.prototype.stopWalking = function() {
+Pokemon.prototype.stopWalking = function () {
 	clearInterval(this.walker);
 };
 
@@ -230,8 +230,8 @@ Pokemon.prototype.stopWalking = function() {
  * startAnimating()
  * begin cycling timer for walking animation
  */
-Pokemon.prototype.startAnimating = function() {
+Pokemon.prototype.startAnimating = function () {
 	this.direction = this.keycode.LEFT;
-	setInterval(function(ref) { ref.animate(); }, 300, this);
-	setTimeout(function(ref) { ref.takeStep(); }, rand(1000,2000), this);
+	setInterval(function (ref) { ref.animate(); }, 300, this);
+	setTimeout(function (ref) { ref.takeStep(); }, rand(1000, 2000), this);
 };
--- js/terminal.js
+++ js/terminal.js
@@ -1,50 +1,50 @@
 
 var Terminal = {
 
-   userName:"",
-   text:"",              // to contain the content of the text file
-   index:0,              // current cursor position in the text to add
-   speed:1,              // number of letters to add at a time
-   file:"",              // name of text file to get text from
-   pending:"",           // pending string of text to type
-   insertingHtml:false,  // indicates if we're inserting text between html tags
-   contentOffset:0,      // the offset in the content compared to the textfile
-   pauseDuration:500,    // duration of pause action in milliseconds
-   htmlIndicator:"<",    // character indicating that we have encountered html
-   acceptingInput:false, // indicating if accepting user input
-   input:"",             // input text from user
-   skip:false,           // controls skipping typing animation
+   userName: "",
+   text: "",              // to contain the content of the text file
+   index: 0,              // current cursor position in the text to add
+   speed: 1,              // number of letters to add at a time
+   file: "",              // name of text file to get text from
+   pending: "",           // pending string of text to type
+   insertingHtml: false,  // indicates if we're inserting text between html tags
+   contentOffset: 0,      // the offset in the content compared to the textfile
+   pauseDuration: 500,    // duration of pause action in milliseconds
+   htmlIndicator: "<",    // character indicating that we have encountered html
+   acceptingInput: false, // indicating if accepting user input
+   input: "",             // input text from user
+   skip: false,           // controls skipping typing animation
 
    // keycode constants
-   keycode:Object.freeze({ENTER: 13, BACKSPACE: 8, SPACE: 32}),
+   keycode: Object.freeze({ ENTER: 13, BACKSPACE: 8, SPACE: 32 }),
 
    // these characters are interpreted as special actions when read
    // i.e. linebreak, pause, clear, preformatted message
-   specialCharacters:["\n", "\\", "`", "$"],
+   specialCharacters: ["\n", "\\", "`", "$"],
 
    /**
     * init()
     * Starts cursor blinking, reads textfile and stores in Terminal.text
     * Adds click events to action links
     */
-   init:function() {
-      setInterval(function(){Terminal.blink();},500); // start cursor blink
+   init: function () {
+      setInterval(function () { Terminal.blink(); }, 500); // start cursor blink
 
-      $.get(Terminal.file,function(data){
+      $.get(Terminal.file, function (data) {
          Terminal.text = data; // save the textfile in Terminal.text
       });
 
       Terminal.addInputListener();
 
       //add click actions for links before they're even added
-      $(window).on('load', function() {
-         $("#console").on('click', "#one", function() {
+      $(window).on('load', function () {
+         $("#console").on('click', "#one", function () {
             Terminal.clearInput();
             Terminal.write('\n');
             Terminal.acceptingInput = false;
             Terminal.printResume();
          });
-         $("#console").on('click', "#three", function() {
+         $("#console").on('click', "#three", function () {
             Terminal.clearInput();
             Terminal.write('3');
             Terminal.acceptingInput = false;
@@ -58,8 +58,8 @@ var Terminal = {
     * startTyping()
     * begins the Terminal's process of automatically typing text
     */
-   startTyping:function() {
-      Terminal.typer = setInterval(function() {Terminal.type();}, 1);
+   startTyping: function () {
+      Terminal.typer = setInterval(function () { Terminal.type(); }, 1);
    },
 
    /**
@@ -67,15 +67,15 @@ var Terminal = {
     * repeating function which keeps adding text as long as there
     * is text left to type
     */
-   type:function() {
+   type: function () {
       Terminal.addText();
       if (Terminal.index > Terminal.text.length) {
          Terminal.stopTyping();
       }
 
-      if(Terminal.skip) {
+      if (Terminal.skip) {
          Terminal.stopTyping();
-         while(Terminal.index <= Terminal.text.length) {
+         while (Terminal.index <= Terminal.text.length) {
             Terminal.addText();
          }
          Terminal.skip = false;
@@ -86,7 +86,7 @@ var Terminal = {
     * stopTyping()
     * pause the Terminal's typing
     */
-   stopTyping:function() {
+   stopTyping: function () {
       clearInterval(Terminal.typer);
    },
 
@@ -94,7 +94,7 @@ var Terminal = {
     * content()
     * returns the content in the terminal
     */
-   content:function() {
+   content: function () {
       return $("#text").html();
    },
 
@@ -102,7 +102,7 @@ var Terminal = {
     * write(str)
     * appends str to the end of the terminal
     */
-   write:function(str) {
+   write: function (str) {
       Terminal.removeCursor();
       // $("#console").append(str);
       $("#text").append(str);
@@ -114,7 +114,7 @@ var Terminal = {
     * inserts strings into the current place of the cursor
     * this is primarily used for inserting text between html tags
     */
-   insert:function(str) {
+   insert: function (str) {
       Terminal.removeCursor();
       var content = Terminal.content();
       var contentBefore =
@@ -129,7 +129,7 @@ var Terminal = {
     * clear()
     * clears the terminal of all text and html
     */
-   clear:function() {
+   clear: function () {
       $("#text").html("");
    },
 
@@ -139,75 +139,75 @@ var Terminal = {
     * adds keyboard listener to allow input to the terminal
     * also adds listener to allow for control of the sprite
     */
-   addInputListener:function() {
+   addInputListener: function () {
       // keyboard listener for terminal input
-      document.addEventListener("keydown", function(event) {
-            if(Terminal.acceptingInput) {
-               character = String.fromCharCode(event.which).toLowerCase();
+      document.addEventListener("keydown", function (event) {
+         if (Terminal.acceptingInput) {
+            character = String.fromCharCode(event.which).toLowerCase();
 
-               // User hit enter
-               if (event.which == Terminal.keycode.ENTER) {
-                  event.preventDefault();
-                  Terminal.acceptingInput = false;
-                  Terminal.contentOffset--;
-                  if (Terminal.input == "1") {
-                     Terminal.input = "";
-                     Terminal.printResume();
-                  }
-                  else if (Terminal.input == "2") {
-                     Terminal.showMessage(2);
-                     Terminal.downloadResume();
-                  }
-                  else if (Terminal.input == "3") {
-                     Terminal.showMessage(3);
-                  }
-                  else if (Terminal.input == "pokemon") {
-                     Terminal.showMessage(4);
-                     createAllPokemon();
-                  }
-                  else {
-                     Terminal.showMessage(-1);
-                  }
+            // User hit enter
+            if (event.which == Terminal.keycode.ENTER) {
+               event.preventDefault();
+               Terminal.acceptingInput = false;
+               Terminal.contentOffset--;
+               if (Terminal.input == "1") {
+                  Terminal.input = "";
+                  Terminal.printResume();
                }
-
-               // User hit backspace
-               else if (event.which == Terminal.keycode.BACKSPACE) {
-                  event.preventDefault();
-                  if(Terminal.input.length > 0) {
-                     Terminal.removeCursor();
-                     Terminal.removeLastCharacter();
-                     Terminal.contentOffset--;
-                     length = Terminal.input.length;
-                     Terminal.input = Terminal.input.substring(0, length - 1);
-                  }
+               else if (Terminal.input == "2") {
+                  Terminal.showMessage(2);
+                  Terminal.downloadResume();
                }
+               else if (Terminal.input == "3") {
+                  Terminal.showMessage(3);
+               }
+               else if (Terminal.input == "pokemon") {
+                  Terminal.showMessage(4);
+                  createAllPokemon();
+               }
+               else {
+                  Terminal.showMessage(-1);
+               }
+            }
 
-               // User hit an alpha-numeric key
-               else if ((character >= 0 && character <= 9) ||
-                        (character >= 'a' && character <= 'z')) {
+            // User hit backspace
+            else if (event.which == Terminal.keycode.BACKSPACE) {
+               event.preventDefault();
+               if (Terminal.input.length > 0) {
                   Terminal.removeCursor();
-                  Terminal.write(character);
-                  Terminal.input += character;
-                  Terminal.contentOffset++;
+                  Terminal.removeLastCharacter();
+                  Terminal.contentOffset--;
+                  length = Terminal.input.length;
+                  Terminal.input = Terminal.input.substring(0, length - 1);
                }
             }
-            // skip animation
-            else {
-               if(event.which == Terminal.keycode.SPACE) {
-                  Terminal.skip = true;
-               }
+
+            // User hit an alpha-numeric key
+            else if ((character >= 0 && character <= 9) ||
+               (character >= 'a' && character <= 'z')) {
+               Terminal.removeCursor();
+               Terminal.write(character);
+               Terminal.input += character;
+               Terminal.contentOffset++;
             }
+         }
+         // skip animation
+         else {
+            if (event.which == Terminal.keycode.SPACE) {
+               Terminal.skip = true;
+            }
+         }
       });
    },
 
-   clearInput:function() {
+   clearInput: function () {
       Terminal.offset -= Terminal.input.length;
       Terminal.input = "";
    },
 
-   showMessage:function(code) {
+   showMessage: function (code) {
       Terminal.text += " \n";
-      switch(code) {
+      switch (code) {
          // download resume
          case 2:
             Terminal.text += "downloading resume...";
@@ -216,7 +216,7 @@ var Terminal = {
          // contact me
          case 3:
             Terminal.text += "you can contact me at "
-                          +  "<a class=\"c\" href=\"mailto:cristianlara@alumni.stanford.edu\">cristianlara@alumni.stanford.edu</a>";
+               + "<a class=\"c\" href=\"mailto:cristianlara@alumni.stanford.edu\">cristianlara@alumni.stanford.edu</a>";
             break;
 
          // pokemon
@@ -227,7 +227,7 @@ var Terminal = {
          // error code
          case (-1):
             Terminal.text += "<span class=\"error\">"
-                          +  "enter number between 1 and 3...</span>";
+               + "enter number between 1 and 3...</span>";
             break;
 
          default:
@@ -238,11 +238,11 @@ var Terminal = {
       Terminal.clearInput();
    },
 
-   downloadResume:function() {
+   downloadResume: function () {
       document.getElementById("two").click();
    },
 
-   emailMe:function() {
+   emailMe: function () {
       document.getElementById("three").click();
    },
 
@@ -255,27 +255,27 @@ var Terminal = {
     * `  : pause before typing more
     * \  : clear terminal
     */
-   handleSpecialCharacter:function(char) {
-      if(char.includes("\n")) {
+   handleSpecialCharacter: function (char) {
+      if (char.includes("\n")) {
          Terminal.insert("<br>");
          Terminal.contentOffset += "<br>".length - "\n".length;
       }
-      else if(char.includes("$")) {
-         var prompt = "<span class=\"a\">root@" +Terminal.userName +"</span>:"+
-                      "<span class=\"b\">~</span>" +
-                      "<span class=\"c\">$</span> ";
+      else if (char.includes("$")) {
+         var prompt = "<span class=\"a\">root@" + Terminal.userName + "</span>:" +
+            "<span class=\"b\">~</span>" +
+            "<span class=\"c\">$</span> ";
          Terminal.insert(prompt);
          Terminal.contentOffset += prompt.length - "~".length;
-         if(Terminal.index > 3) Terminal.acceptingInput = true;
+         if (Terminal.index > 3) Terminal.acceptingInput = true;
       }
-      else if(char.includes("`")) {
+      else if (char.includes("`")) {
          Terminal.contentOffset -= "`".length;
-         if(!Terminal.skip) {
+         if (!Terminal.skip) {
             Terminal.stopTyping();
             setTimeout(Terminal.startTyping, Terminal.pauseDuration);
          }
       }
-      else if(char.includes("\\")) {
+      else if (char.includes("\\")) {
          Terminal.clear();
          Terminal.contentOffset = -Terminal.index;
       }
@@ -286,8 +286,8 @@ var Terminal = {
     * Adds encountered html tags to the content, then offsets index to add
     * next characters between the tags until the end tag is reached.
     */
-   handleHtml:function() {
-      if(Terminal.insertingHtml) {
+   handleHtml: function () {
+      if (Terminal.insertingHtml) {
          Terminal.insertingHtml = false;
 
          var endOfCloseTag = Terminal.text.indexOf(">", Terminal.index);
@@ -301,8 +301,8 @@ var Terminal = {
          var startOfCloseTag = Terminal.text.indexOf("<", endOfOpenTag);
          var endOfCloseTag = Terminal.text.indexOf(">", startOfCloseTag);
 
-         var openTag=Terminal.text.substring(Terminal.index-1,endOfOpenTag+1);
-         var closeTag=Terminal.text.substring(startOfCloseTag,endOfCloseTag+1);
+         var openTag = Terminal.text.substring(Terminal.index - 1, endOfOpenTag + 1);
+         var closeTag = Terminal.text.substring(startOfCloseTag, endOfCloseTag + 1);
 
          Terminal.insert(openTag + closeTag);
 
@@ -315,19 +315,19 @@ var Terminal = {
     * Goes through text file typing it into the Terminal one letter at a time.
     * Recognizes html and adds the tags before typing out the inner content.
     */
-   addText:function() {
-      if(!Terminal.text) return;
+   addText: function () {
+      if (!Terminal.text) return;
       Terminal.removeCursor();
 
       Terminal.index += Terminal.speed;
       Terminal.pending = Terminal.text.charAt(Terminal.index - 1);
 
       // if the next character indicates an action
-      if(Terminal.specialCharacters.includes(Terminal.pending)) {
+      if (Terminal.specialCharacters.includes(Terminal.pending)) {
          Terminal.handleSpecialCharacter(Terminal.pending);
       }
       // if the next character is an html tag
-      else if(Terminal.pending == (Terminal.htmlIndicator)) {
+      else if (Terminal.pending == (Terminal.htmlIndicator)) {
          Terminal.handleHtml();
       }
       // it's just a normal character to insert
@@ -335,12 +335,12 @@ var Terminal = {
          Terminal.insert(Terminal.pending);
       }
 
-      window.scrollBy(0,50); // scroll to make sure bottom is always visible
+      window.scrollBy(0, 50); // scroll to make sure bottom is always visible
 
    },
 
-   printResume:function() {
-      $.get(Terminal.resume,function(resumeData){
+   printResume: function () {
+      $.get(Terminal.resume, function (resumeData) {
          Terminal.text += resumeData;
          Terminal.startTyping();
       });
@@ -350,16 +350,16 @@ var Terminal = {
     * removeLastCharacter()
     * removes the last character in the Terminal's content
     */
-   removeLastCharacter:function() {
-        var content = Terminal.content();
-        $("#text").html(content.substring(0, content.length - 1));
+   removeLastCharacter: function () {
+      var content = Terminal.content();
+      $("#text").html(content.substring(0, content.length - 1));
    },
 
    /**
     * cursorIsOn()
     * returns true if the cursor is being displayed, false otehrwise
     */
-   cursorIsOn:function() {
+   cursorIsOn: function () {
       var content = $("#cursor").html();
       return content == "|";
    },
@@ -368,8 +368,8 @@ var Terminal = {
     * removeCursor()
     * removes the cursor if it is currently being displayed
     */
-   removeCursor:function(){
-      if(Terminal.cursorIsOn())
+   removeCursor: function () {
+      if (Terminal.cursorIsOn())
          $("#cursor").html("");
    },
 
@@ -377,17 +377,17 @@ var Terminal = {
     * blink()
     * blink the cursor by removing and reading it
     */
-   blink:function(){
+   blink: function () {
       var content = Terminal.content();
-      if(Terminal.cursorIsOn())
+      if (Terminal.cursorIsOn())
          $("#cursor").html("");
       else
          $("#cursor").html("|");
    }
 }
 
-Terminal.file="text/intro.txt";
-Terminal.resume="text/resume.txt";
+Terminal.file = "text/intro.txt";
+Terminal.resume = "text/resume.txt";
 Terminal.userName = "cristianlara"
 
 Terminal.init();
